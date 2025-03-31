2a49fa9f580470c1dc5fb5d1b073741050c59da9
tmont2 <tmont2@gmail.com>
2025-03-30 22:59:42 -0700

    DinoPass generator

---
+++ Generate-DinoPassword.ps1
@@ -0,0 +1,34 @@
+﻿<#
+  .SYNOPSIS
+  Generate 1 or more passwords using DinoPass API
+
+  .DESCRIPTION
+  Generate 1 or more passwords using DinoPass API
+  Specify Number to get the number of passwords desired
+  Specify Path to output the passwords to a file
+
+#>
+# function Generate-DinoPassword {
+    [CmdletBinding()]
+    param(
+        $Number = 1,
+        $Path = ""
+    )
+    $counter = 1    
+    $uri = "https://www.dinopass.com/password/strong"
+    $PasswordList = while ($counter -le $Number) {
+        $response = Invoke-WebRequest -URI $uri -Method Get
+        if ($response.StatusCode -eq 200) {
+            $counter += 1
+        }
+        $response.Content
+        Start-Sleep -Seconds 1
+    }
+
+    
+    $PasswordList | Out-File -FilePath $Path
+    #endregion
+# }
+
+# Example 1
+# Generate-DinoPassword -Number 3 -Path "/home/terrence/Desktop/PassList.txt"
---
+++ Reset-EntraPasswords.ps1
@@ -0,0 +1,51 @@
+﻿#region Change Password and Sign this user out of all Microsoft 365 sessions.
+# script to reset Entra ID user passwords uses a CSV file with columns UPN and NewPassword
+$Scopes = "User.ReadWrite.All"
+Connect-MgGraph -Scopes $Scopes -UseDeviceCode
+$ReportList = [System.Collections.Generic.List[Object]]::New()
+$CsvImportPath = "/home/terrence/Desktop/VSCode/ResetPassword.csv"
+$CsvExportPath = "/home/terrence/Desktop/VSCode/ResetPasswordResult.csv"
+
+$UserList = Import-CSV $CsvImportPath
+foreach($User in $UserList) { 
+    $PwResetStatus, $UserSignOutStatus = "Success", "Success"
+
+    $PasswordProfile = @{  
+        ForceChangePasswordNextSignIn = $false 
+        Password = $User.NewPassword 
+    } 
+    # use a try/catch to reset the password
+    try { 
+        Update-MgUser -UserId $User.UPN -PasswordProfile $PasswordProfile -ErrorAction Stop 
+        Write-Host -ForegroundColor Green "Password updated for $($User.UPN)" 
+    } 
+    catch { 
+        $errorMessage = $_.Exception.Message 
+        Write-Host -ForegroundColor Red "Failed to update the password for $($User.UPN): $($errorMessage)"
+        $PwResetStatus = "Fail"
+    } 
+    # use a try/catch to sign out all user sessions
+    try {
+        $SignOutResult = Revoke-MgUserSignInSession -UserId $User.UPN -ErrorAction Stop
+        if ($SignOutResult.Value -eq $true) {
+            Write-Host -ForegroundColor Green "Sign out succeeded for $($User.UPN)" 
+        } else {
+            Write-Host -ForegroundColor Red "Sign out failed for $($User.UPN)"
+            $UserSignOutStatus = "Fail"
+        }     
+    }
+    catch {
+        Write-Host -ForegroundColor Red "Sign out failed for $($User.UPN)"
+        $UserSignOutStatus = "Fail"
+    }
+    $ReportLine = [PSCustomObject]@{
+        UserName = $User.UPN
+        PasswordReset = $PwResetStatus
+        UserSignedOut = $UserSignOutStatus
+    }
+    $ReportList.Add($ReportLine)
+} 
+$ReportList | Sort-Object -Property Username | Format-Table
+$ReportList | Sort-Object -Property Username | Export-Csv -Path $CsvExportPath -Encoding utf8 -NoTypeInformation
+
+#endregion
